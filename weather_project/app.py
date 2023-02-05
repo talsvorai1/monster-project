7347e77614598c30364e40a5736659062b8fad63
tal.svorai <talsvorai@gmail.com>
2023-02-05 11:21:12 +0200

    moving traffic to port 80

--- weather_project/app.py
+++ weather_project/app.py
@@ -52,5 +52,5 @@ def index():                                             #creating index view fi
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0', port=80)
 
